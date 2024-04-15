e5c69fc2444ddaa073b849edac65a4f4f9569b4a
COLI2\breanna.ho <breanna.ho@kbadmin.com>
2024-04-15 15:51:21 -0400

    End of section 7

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,5 @@
 using API.Extentions;
-
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +12,8 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.UseAuthentication(); 
@@ -19,7 +21,6 @@ app.UseAuthorization();
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
 
 app.MapControllers();
 
