0203e3fc4f3f55ce7e3f78e8a56ea36c34f7bb00
COLI2\breanna.ho <breanna.ho@kbadmin.com>
2024-04-22 11:15:55 -0400

    End of section 15

--- API/Extentions/ApplicationServiceExtentions.cs
+++ API/Extentions/ApplicationServiceExtentions.cs
@@ -22,6 +22,7 @@ namespace API.Extentions
             services.AddScoped<IPhotoService, PhotoService>();
             services.AddScoped<LogUserActivity>();
             services.AddScoped<ILikesRepository, LikesRepository>();
+            services.AddScoped<IMessageRepository, MessageRepository>();
 
             return services; 
         }
